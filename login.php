f2ddd017c6577886ee47f7eae5baad0ea65a1f84
MatthiasPL <smateusz96@gmail.com>
2019-01-06 15:05:50 +0100

    added login

--- login.php
+++ login.php
@@ -28,46 +28,59 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="order.php">Order</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Menu
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="menu.php">All menu items</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="menu.php">Vegan friendly</a>
-                        <a class="dropdown-item" href="menu.php">Gluten free</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="staff.php">Our staff</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="contact.php">Contact</a>
-                </li>
-            </ul>
-        </div>
-
-        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item my-2 my-lg-0">
-                    <a class="nav-link" href="login.php">Log in</a>
-                </li>
-            </ul>
-        </div>
-
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="order.php">Order</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Menu
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="menu.php">All menu items</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="menu.php">Vegan friendly</a>
+                    <a class="dropdown-item" href="menu.php">Gluten free</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="staff.php">Our staff</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="login.php">Log in</a>
+            </li>
+        </ul>
     </div>
 </nav>
 <div class="container">
-
+    <div class="row">
+        <div class="col-md-4 col-lg-4 col-sm-1"></div>
+        <div class="col-md-4 col-lg-4 col-sm-10">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title"><strong>Sign In </strong></div>
+                    <form role="form">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">E-mail:</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter e-mail">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password:</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter password">
+                        </div>
+                        <button type="submit" class="btn btn-primary float-right">Sign in</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-lg-4 col-sm-1"></div>
+    </div>
 </div>
 
 <script src="js/jquery-3.3.1.min.js"></script>
